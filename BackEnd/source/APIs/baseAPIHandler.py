d8f1e7973496b62baa24519ff0ff93bf1898d883
Antonio Reyes Lua <t7.reyeslua@gmail.com>
2016-04-23 20:06:03 +0200

    [baseAPI] Remove unecessary stuff

--- BackEnd/source/APIs/baseAPIHandler.py
+++ BackEnd/source/APIs/baseAPIHandler.py
@@ -1,6 +1,6 @@
 import logging
-import psycopg2
-from user import get_user
+# import psycopg2
+# from user import get_user
 
 
 class BaseAPIHandler:
@@ -21,18 +21,6 @@ class BaseAPIHandler:
         Find the right handler and distribute the incoming message to there.
         '''
         try:
-            if user is None:
-                if client.user is not None:
-                    user = client.user
-            else:
-                user = get_user(user)
-
-            if not self.authenticate(client, msgid, data, user, msgtype):
-                return (self.name, None, 'error',
-                        {'msg': 'User not authenticated to use function ' +
-                                'in API ' + self.name},
-                        msgid)
-
             func = self.message_handlers[msgtype]
             try:
                 return func(client, msgid, data, user)
@@ -50,24 +38,6 @@ class BaseAPIHandler:
                 logging.exception('AttributeError inside function: ' + str(e))
                 return (self.name, None, 'error',
                         {'msg': 'Invalid command syntax'},
-                        msgid)
-            except psycopg2.ProgrammingError as e:
-                e.cursor.connection.rollback()
-                logging.error('Database error: ' + str(e))
-                return (self.name, None, 'error',
-                        {'msg': 'Database error "' + str(e) + '"'},
-                        msgid)
-            except psycopg2.DataError as e:
-                e.cursor.connection.rollback()
-                logging.error('Database error: ' + str(e))
-                return (self.name, None, 'error',
-                        {'msg': 'Invalid value provided'},
-                        msgid)
-            except psycopg2.OperationalError as e:
-                logging.error('Database error: ' + str(e))
-                return (self.name, None, 'error',
-                        {'msg': 'Internal error. Please notify system ' +
-                                'manager with notice: DB unreachable'},
                         msgid)
         except KeyError:
             logging.error('Unknown messagetype %s' % msgtype)
