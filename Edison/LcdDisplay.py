45914910ccee6e7699dda00a0fc9b61344241706
Adithya Pulli <adithya.pulli@gmail.com>
2016-04-23 20:03:36 +0000

    Refined model. Working test.

--- Edison/LcdDisplay.py
+++ Edison/LcdDisplay.py
@@ -26,31 +26,50 @@ import time
 
 bindings = [{'MAC': 123456, "ID": 1, "name": "shoe", "group": ["gym"]},{'MAC': 123456, "ID": 2, "name": "earplugs", "group": ["gym","work"]}, {'MAC': 123456, "ID": 3, "name": "keys", "group": ["gym", "work"]}, {'MAC': 123456, "ID": 4, "name": "laptop", "group": ["work"]}]
 
+def clear_display():
+    myLcd.setColor(0, 0, 0)
+    myLcd.setCursor(0,0)
+    myLcd.write('                       ')
+    myLcd.setCursor(1,0)
+    myLcd.write('                       ')
+
 def update_display(scenario, ids_in_network):
-	myLcd.setCursor(0,0)
-	print "Scenario "+scenario
-	print ids_in_network
-	required_nodes = [x["ID"] for x in bindings if scenario in x["group"]]
-	ids_not_in_network = [x for x in required_nodes if x not in ids_in_network]
-	if(len(ids_not_in_network) == 0):
-		print "All nodes in range"
-		myLcd.setColor(0, 255, 0)
-		myLcd.write('All set! Lets get going.')
-	else:
-		print "Missing nodes in range"
-		print ids_not_in_network
-		myLcd.setColor(255, 0, 0)
-		missing_items = [x["name"] for x in bindings if x["ID"] in ids_not_in_network]
-		myLcd.write('Missing '+" ".join(missing_items))
+    print "Scenario "+scenario
+    print ids_in_network
+    required_nodes = [x["ID"] for x in bindings if scenario in x["group"]]
+    ids_not_in_network = [x for x in required_nodes if x not in ids_in_network]
+    if(len(ids_not_in_network) == 0):
+        print "All nodes in range"
+        clear_display()
+        myLcd.setColor(0, 255, 0)
+        myLcd.setCursor(0,0)
+        myLcd.write('All set!')
+        myLcd.setCursor(1,0)
+        myLcd.write('Lets get going.')
+    else:
+        print "Missing nodes in range"
+        print ids_not_in_network
+        clear_display()
+        myLcd.setColor(255, 0, 0)
+        missing_items = [x["name"] for x in bindings if x["ID"] in ids_not_in_network]
+        myLcd.setCursor(0,0)
+        myLcd.write('Did you forget ')
+        myLcd.setCursor(1,0)
+        myLcd.write(" ".join(missing_items))
 
 # RGB Red
 
 if __name__ == "__main__":
-	# Initialize Jhd1313m1 at 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS) 
-	myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
-	print "LCD Display TEST"
-	while(True):
-		update_display("gym", [1,2])
-		time.sleep(5)
-		update_display("work", [4,2])
-		time.sleep(5)
+    myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+    # Initialize Jhd1313m1 at 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS) 
+    print "LCD Display TEST"
+    update_display("work", [1,2,3,4])
+    time.sleep(2)
+    update_display("work", [1,2])
+    time.sleep(2)
+    update_display("gym", [1,2])
+    time.sleep(2)
+    update_display("gym", [1,2,3,4])
+    time.sleep(2)
+    clear_display()
+    time.sleep(2)
